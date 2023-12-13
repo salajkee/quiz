d74fc0d7732623ec4b4b35fcaed6d111bb22b92c
Salamat <salajkee@mail.ru>
2023-12-13 11:14:37 +0500

    third commit

--- js/script.js
+++ js/script.js
@@ -5,8 +5,10 @@ const sliderSumSum = sliderSum.querySelector('.quiz__slider-bot-sum')
 const sliderSumInput = sliderSum.querySelector('.quiz__slider-range-input')
 const sliderTermSum = sliderTerm.querySelector('.quiz__slider-bot-sum')
 const sliderTermInput = sliderTerm.querySelector('.quiz__slider-range-input')
+const sliderSumProgress = sliderSum.querySelector('progress')
+const sliderTermProgress = sliderTerm.querySelector('progress')
 
-function rangeSlider(sliderInput, sliderSum) {
+function rangeSlider(sliderInput, sliderSum, progress) {
 	const sum = Number(sliderInput.value).toLocaleString()
 	sliderSum.value = sum
 
@@ -16,16 +18,18 @@ function rangeSlider(sliderInput, sliderSum) {
 
 	sliderInput.addEventListener('input', e => {
 		sliderSum.value = Number(e.target.value).toLocaleString()
+		progress.value = e.target.value
 	})
 }
 
-rangeSlider(sliderSumInput, sliderSumSum)
-rangeSlider(sliderTermInput, sliderTermSum)
+rangeSlider(sliderSumInput, sliderSumSum, sliderSumProgress)
+rangeSlider(sliderTermInput, sliderTermSum, sliderTermProgress)
 
 // Quiz Steps
 const quizStep = document.querySelectorAll('.quiz__step')
 const nextBtn = document.querySelector('.quiz__btn')
 const quizStepsItem = document.querySelectorAll('.quiz__steps-item')
+const quizBtn = document.querySelector('.quiz__btn')
 
 let currentStep = 0
 
@@ -44,6 +48,10 @@ nextBtn.addEventListener('click', () => {
 			item.classList.add('quiz__steps-item--done')
 		}
 	})
+
+	if (currentStep == 4) {
+		quizBtn.classList.add('hide')
+	}
 
 	quizStep[currentStep].classList.add('quiz__step--active')
 	quizStepsItem[currentStep].classList.add('quiz__steps-item--active')
